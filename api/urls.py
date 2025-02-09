c0ae5e470b8905672d519d0579900d08a91c8e21
Kartiqus <vicaire.des.ombres@gmail.com>
2025-02-09 05:35:01 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet
+from django.urls import path
+from .views import admin_login
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -9,4 +11,5 @@ router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/login/', admin_login, name='admin_login'),
 ]
