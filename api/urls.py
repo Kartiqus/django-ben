7240946042f9b32cd141d1fcbb3d447f396aecd7
Kartiqus <vicaire.des.ombres@gmail.com>
2025-02-09 10:37:23 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,17 @@
-# api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, OrderViewSet
-from django.urls import path
-from .views import admin_login
+from .views import CustomUserViewSet, ProductViewSet, CartViewSet, OrderViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
+router.register(r'users', CustomUserViewSet)
 router.register(r'products', ProductViewSet)
-router.register(r'orders', OrderViewSet)
+router.register(r'carts', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('admin/login/', admin_login, name='admin_login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
